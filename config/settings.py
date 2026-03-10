164242328ffa8aa654eb566f33a7293259f2f98c
AdrJM <adrianjasek04@gmail.com>
2026-03-10 11:11:33 +0100

    feat: add .env support for configuration

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,7 @@
-MODEL_SIZE = "medium"
-LANGUAGE = "pl"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MODEL_SIZE = os.getenv("MODEL_SIZE", "medium")
+LANGUAGE = os.getenv("LANGUAGE", "pl")
